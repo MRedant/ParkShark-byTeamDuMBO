4c19f2badc167111dc5c71e35c0d34a3431e593a
BruCuCoder <brecht@superstudio.be>
2018-04-10 15:18:11 +0200

    update SQL statements

--- domain/src/main/java/be/dumbo/switchfully/parkshark/domain/division/Division.java
+++ domain/src/main/java/be/dumbo/switchfully/parkshark/domain/division/Division.java
@@ -1,19 +1,23 @@
 package be.dumbo.switchfully.parkshark.domain.division;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 @Entity
 @Table(name="DIVISION")
 public class Division {
 
     @Id
+    @Column(name="ID")
+    @SequenceGenerator(name="division_seq", sequenceName = "DIVISION_SEQ", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "division_seq")
     private Integer id;
+    @Column(name="NAME")
     private String name;
+    @Column(name="ORIGINALNAME")
     private String originalName;
+    @Column(name="DIRECTOR")
     private String director;
+    @Column(name="PARENTDIVISION")
     private Integer parentDivision;
 
     private Division() {
