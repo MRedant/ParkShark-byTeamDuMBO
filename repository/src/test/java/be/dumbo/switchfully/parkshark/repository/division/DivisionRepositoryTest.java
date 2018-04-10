3884aa2ac482f426f939966e43736bdb7450b2b5
BruCuCoder <brecht@superstudio.be>
2018-04-10 16:52:24 +0200

    in between push

---
+++ repository/src/test/java/be/dumbo/switchfully/parkshark/repository/division/DivisionRepositoryTest.java
@@ -0,0 +1,29 @@
+package be.dumbo.switchfully.parkshark.repository.division;
+
+import be.dumbo.switchfully.parkshark.domain.division.Division;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(Parksharkconfig.class)
+public class DivisionRepositoryTest {
+
+    private DivisionRepository divisionRepository;
+
+    @Autowired
+    DivisionRepositoryTest(DivisionRepository divisionRepository) {
+        this.divisionRepository = divisionRepository;
+    }
+
+    @Test
+    void save_createDivisionReturnsDivisionWithId() {
+        //GIVEN
+        Division division = new Division("DivisionABC","SomeOldName","Maarten Supreme Leader");
+        //WHEN
+        Integer id = divisionRepository.save(division).getId();
+        //THEN
+        Assertions.assertThat(id).isNotNull();
+
+    }
+}
