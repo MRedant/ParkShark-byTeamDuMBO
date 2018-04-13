d17185dfa8667e6dcd08e4fa13e75aa78f6d8119
BruCuCoder <brecht@superstudio.be>
2018-04-13 15:44:39 +0200

    start integration test division

---
+++ service/src/test/java/be/dumbo/switchfully/parkshark/service/division/DivisionServiceIntegrationTest.java
@@ -0,0 +1,16 @@
+package be.dumbo.switchfully.parkshark.service.division;
+
+import org.junit.Test;
+
+public class DivisionServiceIntegrationTest {
+
+    @Test
+    public void getAll_givenADivisionTable_returnListOfAllDivisions() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+
+}
