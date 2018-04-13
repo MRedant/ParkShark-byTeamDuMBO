da6148efb0b40d39cb41cadda3c4da1fc765d330
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-04-13 18:31:19 +0200

    working in division api

--- repository/src/test/java/be/dumbo/switchfully/parkshark/repository/division/DivisionRepositoryTest.java
+++ repository/src/test/java/be/dumbo/switchfully/parkshark/repository/division/DivisionRepositoryTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=Application.class)
+@EntityScan
 public class DivisionRepositoryTest {
 
     @Autowired
