b611887c11b92ea8b5c69d14c0e75d3ee290b831
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-04-14 06:40:36 +0200

    delete jar from project

--- repository/src/test/java/be/dumbo/switchfully/parkshark/repository/division/DivisionRepositoryTest.java
+++ repository/src/test/java/be/dumbo/switchfully/parkshark/repository/division/DivisionRepositoryTest.java
@@ -6,7 +6,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,7 +13,6 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=Application.class)
-@EntityScan
 public class DivisionRepositoryTest {
 
     @Autowired
